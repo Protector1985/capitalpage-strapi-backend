2915141d31fd99ddc044f903657883234064c903
Protector1985 <michaelrbraun1985@gmail.com>
2022-06-11 13:42:33 -0700

    created custom routing for to accept slug in query - for SEO

--- src/api/post/controllers/post.js
+++ src/api/post/controllers/post.js
@@ -6,4 +6,18 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::post.post');
+module.exports = createCoreController('api::post.post', ({strapi}) => ({
+    //custom code to be able to find route by slug 
+    async findOne(context) {
+        
+        const { id } = context.params;
+        console.log(id);
+        const entity = await strapi.db.query("api::post.post").findOne({
+            where: { slug: id }
+        })
+
+        const sanitizedEntity = await this.sanitizeOutput(entity);
+
+        return this.transformResponse(sanitizedEntity);
+    }
+}));
